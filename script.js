1403119324100bd8f533aac634787a4552facb82
ghislain kabangu <142664005+ghislainkabangu@users.noreply.github.com>
2025-11-16 16:24:40 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 /** @format */
 
-let header = document.getElementsByTagName("header")[0];
-header.addEventListener("mouseover", () => {
-  header.classList.add("blocFontNewClasse");
+let boutonMenu = document.querySelector(".boutonMenu");
+let closeMenu = document.querySelector(".closeMenu");
+let mobile_navigation = document.querySelector(".mobile_navigation");
+
+boutonMenu.addEventListener("click", () => {
+  mobile_navigation.classList.add("mobile_navigation_js_class");
 });
+closeMenu.addEventListener("click", () => {
+  mobile_navigation.classList.remove("mobile_navigation_js_class");
+});
+
+let linkMobile = document.querySelectorAll(".linkMobile");
+for (let i = 0; i < linkMobile.length; i++) {
+  linkMobile[i].addEventListener("click", () => {
+    mobile_navigation.classList.remove("mobile_navigation_js_class");
+  });
+}
